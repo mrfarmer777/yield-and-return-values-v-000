b85efd39037e9676df96cae89ac668db60763871
Matt Farmer <mrfarmer777@gmail.com>
2018-05-27 18:00:40 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,11 +3,11 @@ require 'pry'
 def hello(array)
   i = 0
   results = []
-  while i < array.length    
+  while i < array.length
     results<<yield(array[i])
     i += 1
   end
   results
 end
 
-
+hello(['Tim',"Tom","Jim"]) {|name| "Hi, #{name}"}
